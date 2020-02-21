2f2700713e93a1cac9264085036548c837e685b3
Florin Marius Drilea <fmdrilea1@sheffield.ac.uk>
2020-02-21 16:22:41 +0000

    added comments

--- part1.py
+++ part1.py
@@ -1,10 +1,13 @@
+# libraries
 import sys
 import requests
 from operator import itemgetter
 
+# get the api
 baseUrl = "https://techtest.rideways.com/"
-suppliers = ["dave", "eric", "hef"]
+suppliers = ["dave", "eric", "jeff"]
 
+# all possible cars + number of people
 cars = {
 	'STANDARD' : 4,
 	'EXECUTIVE' : 4,
@@ -13,11 +16,14 @@ cars = {
 	'LUXURY_PEOPLE_CARRIER' : 6,
 	'MINIBUS' : 16,
 }
+
+# variables
 Dave = True
 Eric = True
 Jeff = True
 options = []
 
+# error if not all parameteres are inserted
 if (len(sys.argv)) < 3:
 	print("Error")
 	sys.exit(0)
@@ -30,13 +36,16 @@ print("Pickup: " + pickup)
 print("Dropoff: " + dropoff)
 print("Passengers: " + passengers)
 
+# searching through all the possible cars from all the apis
+# and keep in mind what is correct in the given order
+
 urlEric = baseUrl + suppliers[1] + "?pickup=" + pickup + "&dropoff=" + dropoff
 
 try:
     requestEric = requests.get(urlEric, timeout=1)
     jsonEric = requestEric.json()
 except (requests.exceptions.ConnectTimeout,requests.exceptions.ReadTimeout) as e:
-    print("Skipping Eric - ")
+	print("Skipping Eric - ")
 	print(e)
 	Eric = False
 	
@@ -46,7 +55,7 @@ try:
     requestJeff = requests.get(urlJeff, timeout=1)
     jsonJeff = requestJeff.json()
 except (requests.exceptions.ConnectTimeout,requests.exceptions.ReadTimeout) as e:
-    print("Skipping supplier Jeff - ")
+	print("Skipping supplier Jeff - ")
 	print(e)
 	Jeff = False
 
@@ -56,11 +65,13 @@ try:
     requestDave = requests.get(urlDave, timeout=1)
     jsonDave = requestDave.json()
 except (requests.exceptions.ConnectTimeout,requests.exceptions.ReadTimeout) as e:
-    print("Skipping Dave - ")
+	print("Skipping Dave - ")
 	print(e)
 	Dave = False
 
-if Eric):
+# get suppliers or get errors for every apis
+
+if Eric:
     if "error" in jsonEric:
         print("Eric api error : " + jsonEric['error'])
         Eric = False
@@ -91,10 +102,13 @@ if Dave:
         options = options + optionDave
 
 
+# go through all the options that were selected above
 branch = {branch['car_type']:branch for branch in options}.values()
 
+# arrange them in ascending order
 branchSorted = sorted(branch, reverse=True, key=itemgetter("price"))
 
+# print all options possible in ascending order
 for option in branchSorted:
     if (int(cars[option['car_type']]) >= int(passengers)):
         print(option['car_type'] + " - " + str(option['supplier']) + " - " + str(option['price']))
--- part2Process.py
+++ part2Process.py
@@ -1,9 +1,13 @@
+# libraries
 import requests
 import sys
 from operator import itemgetter
 
+# get the api
 baseUrl = "https://techtest.rideways.com/"
 suppliers = ["dave", "eric", "jeff"]
+
+# all possible cars + number of people
 cars = {
     'STANDARD' : 4,
     'EXECUTIVE' : 4,
@@ -12,11 +16,14 @@ cars = {
     'LUXURY_PEOPLE_CARRIER' : 6,
     'MINIBUS' : 16,
 }
+
+# variables
 Dave = True
 Eric = True
 Jeff = True
 options = []
 
+# error if not all parameteres are inserted
 if (len(sys.argv)) < 3:
     print("Error")
     sys.exit(0)
@@ -25,7 +32,11 @@ else:
     dropoff = sys.argv[2]
     passengers = sys.argv[3]
 
+# searching through all the possible cars from all the apis
+# and keep in mind what is correct in the given order
+
 formedUrlEric = baseUrl + suppliers[1] + "?pickup=" + pickup + "&dropoff=" + dropoff
+
 try:
     requestEric = requests.get(formedUrlEric, timeout=1)
     jsonEric = requestEric.json()
@@ -33,6 +44,7 @@ except (requests.exceptions.ConnectTimeout,requests.exceptions.ReadTimeout) as e
     Eric = False
 
 formedUrlDave = baseUrl + suppliers[0] + "?pickup=" + pickup + "&dropoff=" + dropoff
+
 try:
     requestDave = requests.get(formedUrlDave, timeout=1)
     jsonDave = requestDave.json()
@@ -40,12 +52,14 @@ except (requests.exceptions.ConnectTimeout,requests.exceptions.ReadTimeout) as e
     Dave = False
 
 formedUrlJeff = baseUrl + suppliers[2] + "?pickup=" + pickup + "&dropoff=" + dropoff
+
 try:
     requestJeff = requests.get(formedUrlJeff, timeout=1)
     jsonJeff = requestJeff.json()
 except (requests.exceptions.ConnectTimeout,requests.exceptions.ReadTimeout) as e:
     Jeff = False
 
+# get suppliers or get errors for every apis
 if Eric:
     if "error" in jsonEric:
         Eric = False
@@ -73,10 +87,14 @@ if Jeff:
             option['supplier'] = "Jeff"
         options = options + optionsJeff
 
-pruned = {pruned['car_type']:pruned for pruned in options}.values()
 
-prunedSort = sorted(pruned, reverse=True, key=itemgetter("price"))
+# go through all the options that were selected above
+branch = {branch['car_type']:branch for branch in options}.values()
 
-for option in prunedSort:
+# arrange them in ascending order
+branchSorted = sorted(branch, reverse=True, key=itemgetter("price"))
+
+# print all options possible in ascending order
+for option in branchSorted:
     if (int(cars[option['car_type']]) >= int(passengers)):
         print(option['car_type'] + " - " + str(option['supplier']) + " - " + str(option['price']))
